9333f43911b074945811ed9b1b465bb6d018bbfa
joseluizs <joseluiz.smarques@gmail.com>
2024-08-26 16:52:03 -0300

    Initial commit

---
+++ UC Teste/teste_calculadora_mstest/Operacoes.cs
@@ -0,0 +1,17 @@
+namespace teste_calculadora_mstest
+{
+    public class Operacoes
+    {
+        public double Somar(double primeiroNumero, double
+        segundoNumero)
+        {
+            return (primeiroNumero + segundoNumero);
+        }
+        public double Multiplicar(double primeiroNumero,
+        double segundoNumero)
+        {
+            return (primeiroNumero * segundoNumero);
+        }
+    }
+}
+
